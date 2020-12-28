649d0fe0b50d1212ed7fa1018da94353eb178e93
Evan Pfeifer <evan.pfeifer@yahoo.ca>
2020-12-27 22:45:09 -0700

    Stack interface

---
+++ src/MyStack/MyStack/HomemadeStack.java
@@ -0,0 +1,14 @@
+package MyStack;
+
+public interface HomemadeStack<T> {
+    /* Remove all elements from the stack */
+    void clear();
+    /* Returns true if the stack contains the specified element */
+    boolean contains(T item);
+    /* Return (but don't remove) the item at the top of the stack */
+    T peek();
+    /* Remove and return the item at the top of the stack */
+    T pop();
+    /* Add an element to the top of the stack */
+    T push(T item);
+}
